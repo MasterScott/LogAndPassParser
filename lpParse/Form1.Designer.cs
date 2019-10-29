f1fbb0c040d6ed645e82fc007246113a0f9913c0
WhoAreY0u-F0rmatC <56751673+f0rmatY0urC@users.noreply.github.com>
2019-10-29 18:18:17 +0300

    Main form

---
+++ lpParse/Form1.Designer.cs
@@ -0,0 +1,297 @@
+﻿namespace lpParse
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.materialLabel1 = new MaterialSkin.Controls.MaterialLabel();
+            this.materialSingleLineTextField1 = new MaterialSkin.Controls.MaterialSingleLineTextField();
+            this.materialLabel2 = new MaterialSkin.Controls.MaterialLabel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.materialLabel3 = new MaterialSkin.Controls.MaterialLabel();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.materialFlatButton2 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialFlatButton3 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialFlatButton4 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.materialFlatButton1 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.materialFlatButton5 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.materialFlatButton6 = new MaterialSkin.Controls.MaterialFlatButton();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // materialLabel1
+            // 
+            this.materialLabel1.AutoSize = true;
+            this.materialLabel1.BackColor = System.Drawing.Color.White;
+            this.materialLabel1.Depth = 0;
+            this.materialLabel1.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel1.Location = new System.Drawing.Point(588, 87);
+            this.materialLabel1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel1.Name = "materialLabel1";
+            this.materialLabel1.Size = new System.Drawing.Size(79, 24);
+            this.materialLabel1.TabIndex = 0;
+            this.materialLabel1.Text = "Запрос:";
+            // 
+            // materialSingleLineTextField1
+            // 
+            this.materialSingleLineTextField1.Depth = 0;
+            this.materialSingleLineTextField1.Hint = "";
+            this.materialSingleLineTextField1.Location = new System.Drawing.Point(673, 87);
+            this.materialSingleLineTextField1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialSingleLineTextField1.Name = "materialSingleLineTextField1";
+            this.materialSingleLineTextField1.PasswordChar = '\0';
+            this.materialSingleLineTextField1.SelectedText = "";
+            this.materialSingleLineTextField1.SelectionLength = 0;
+            this.materialSingleLineTextField1.SelectionStart = 0;
+            this.materialSingleLineTextField1.Size = new System.Drawing.Size(345, 28);
+            this.materialSingleLineTextField1.TabIndex = 1;
+            this.materialSingleLineTextField1.UseSystemPasswordChar = false;
+            // 
+            // materialLabel2
+            // 
+            this.materialLabel2.AutoSize = true;
+            this.materialLabel2.BackColor = System.Drawing.Color.White;
+            this.materialLabel2.Depth = 0;
+            this.materialLabel2.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel2.Location = new System.Drawing.Point(12, 87);
+            this.materialLabel2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel2.Name = "materialLabel2";
+            this.materialLabel2.Size = new System.Drawing.Size(65, 24);
+            this.materialLabel2.TabIndex = 2;
+            this.materialLabel2.Text = "Текст:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(102, 87);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(440, 352);
+            this.textBox1.TabIndex = 3;
+            // 
+            // materialLabel3
+            // 
+            this.materialLabel3.AutoSize = true;
+            this.materialLabel3.BackColor = System.Drawing.Color.White;
+            this.materialLabel3.Depth = 0;
+            this.materialLabel3.Font = new System.Drawing.Font("Roboto", 11F);
+            this.materialLabel3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(222)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.materialLabel3.Location = new System.Drawing.Point(796, 118);
+            this.materialLabel3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialLabel3.Name = "materialLabel3";
+            this.materialLabel3.Size = new System.Drawing.Size(105, 24);
+            this.materialLabel3.TabIndex = 6;
+            this.materialLabel3.Text = "Результат:";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(592, 145);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(521, 294);
+            this.textBox2.TabIndex = 8;
+            // 
+            // materialFlatButton2
+            // 
+            this.materialFlatButton2.AutoSize = true;
+            this.materialFlatButton2.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton2.Depth = 0;
+            this.materialFlatButton2.Location = new System.Drawing.Point(102, 448);
+            this.materialFlatButton2.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton2.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton2.Name = "materialFlatButton2";
+            this.materialFlatButton2.Primary = false;
+            this.materialFlatButton2.Size = new System.Drawing.Size(331, 36);
+            this.materialFlatButton2.TabIndex = 10;
+            this.materialFlatButton2.Text = "Импортировать из архива\\файла";
+            this.materialFlatButton2.UseVisualStyleBackColor = true;
+            this.materialFlatButton2.Click += new System.EventHandler(this.materialFlatButton2_Click);
+            // 
+            // materialFlatButton3
+            // 
+            this.materialFlatButton3.AutoSize = true;
+            this.materialFlatButton3.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton3.Depth = 0;
+            this.materialFlatButton3.Location = new System.Drawing.Point(1025, 82);
+            this.materialFlatButton3.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton3.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton3.Name = "materialFlatButton3";
+            this.materialFlatButton3.Primary = false;
+            this.materialFlatButton3.Size = new System.Drawing.Size(88, 36);
+            this.materialFlatButton3.TabIndex = 11;
+            this.materialFlatButton3.Text = "Отсеять";
+            this.materialFlatButton3.UseVisualStyleBackColor = true;
+            this.materialFlatButton3.Click += new System.EventHandler(this.materialFlatButton3_Click);
+            // 
+            // materialFlatButton4
+            // 
+            this.materialFlatButton4.AutoSize = true;
+            this.materialFlatButton4.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton4.Depth = 0;
+            this.materialFlatButton4.Location = new System.Drawing.Point(438, 448);
+            this.materialFlatButton4.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton4.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton4.Name = "materialFlatButton4";
+            this.materialFlatButton4.Primary = false;
+            this.materialFlatButton4.Size = new System.Drawing.Size(104, 36);
+            this.materialFlatButton4.TabIndex = 12;
+            this.materialFlatButton4.Text = "Очистить";
+            this.materialFlatButton4.UseVisualStyleBackColor = true;
+            this.materialFlatButton4.Click += new System.EventHandler(this.materialFlatButton4_Click);
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.White;
+            this.checkBox1.Location = new System.Drawing.Point(991, 445);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(122, 21);
+            this.checkBox1.TabIndex = 13;
+            this.checkBox1.Text = "Only web-sites";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // materialFlatButton1
+            // 
+            this.materialFlatButton1.AutoSize = true;
+            this.materialFlatButton1.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton1.Depth = 0;
+            this.materialFlatButton1.Location = new System.Drawing.Point(592, 445);
+            this.materialFlatButton1.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton1.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton1.Name = "materialFlatButton1";
+            this.materialFlatButton1.Primary = false;
+            this.materialFlatButton1.Size = new System.Drawing.Size(274, 36);
+            this.materialFlatButton1.TabIndex = 14;
+            this.materialFlatButton1.Text = "Экспорт результата в файл";
+            this.materialFlatButton1.UseVisualStyleBackColor = true;
+            this.materialFlatButton1.Click += new System.EventHandler(this.materialFlatButton1_Click_1);
+            // 
+            // materialFlatButton5
+            // 
+            this.materialFlatButton5.AutoSize = true;
+            this.materialFlatButton5.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton5.Depth = 0;
+            this.materialFlatButton5.Location = new System.Drawing.Point(16, 248);
+            this.materialFlatButton5.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton5.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton5.Name = "materialFlatButton5";
+            this.materialFlatButton5.Primary = false;
+            this.materialFlatButton5.Size = new System.Drawing.Size(69, 36);
+            this.materialFlatButton5.TabIndex = 15;
+            this.materialFlatButton5.Text = "Автор";
+            this.materialFlatButton5.UseVisualStyleBackColor = true;
+            this.materialFlatButton5.Click += new System.EventHandler(this.materialFlatButton5_Click);
+            // 
+            // materialFlatButton6
+            // 
+            this.materialFlatButton6.AutoSize = true;
+            this.materialFlatButton6.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.materialFlatButton6.Depth = 0;
+            this.materialFlatButton6.Location = new System.Drawing.Point(874, 445);
+            this.materialFlatButton6.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.materialFlatButton6.MouseState = MaterialSkin.MouseState.HOVER;
+            this.materialFlatButton6.Name = "materialFlatButton6";
+            this.materialFlatButton6.Primary = false;
+            this.materialFlatButton6.Size = new System.Drawing.Size(104, 36);
+            this.materialFlatButton6.TabIndex = 16;
+            this.materialFlatButton6.Text = "Очистить";
+            this.materialFlatButton6.UseVisualStyleBackColor = true;
+            this.materialFlatButton6.Click += new System.EventHandler(this.materialFlatButton6_Click);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.Color.White;
+            this.checkBox2.Location = new System.Drawing.Point(991, 473);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(98, 21);
+            this.checkBox2.TabIndex = 17;
+            this.checkBox2.Text = "Show Date";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1141, 506);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.materialFlatButton6);
+            this.Controls.Add(this.materialFlatButton5);
+            this.Controls.Add(this.materialFlatButton1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.materialFlatButton4);
+            this.Controls.Add(this.materialFlatButton3);
+            this.Controls.Add(this.materialFlatButton2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.materialLabel3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.materialLabel2);
+            this.Controls.Add(this.materialSingleLineTextField1);
+            this.Controls.Add(this.materialLabel1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(1141, 506);
+            this.MinimumSize = new System.Drawing.Size(1141, 506);
+            this.Name = "Form1";
+            this.Text = "FormatCParser";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private MaterialSkin.Controls.MaterialLabel materialLabel1;
+        private MaterialSkin.Controls.MaterialSingleLineTextField materialSingleLineTextField1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel2;
+        private System.Windows.Forms.TextBox textBox1;
+        private MaterialSkin.Controls.MaterialLabel materialLabel3;
+        private System.Windows.Forms.TextBox textBox2;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton2;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton3;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton4;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton1;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton5;
+        private MaterialSkin.Controls.MaterialFlatButton materialFlatButton6;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ lpParse/Form1.cs
@@ -0,0 +1,217 @@
+﻿using System;
+using System.Data;
+using System.Linq;
+using System.Windows.Forms;
+using MaterialSkin.Controls;
+using System.IO;
+using System.IO.Compression;
+
+namespace lpParse
+{
+    public partial class Form1 : MaterialForm
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Directory.CreateDirectory("Archives\\");
+        }
+
+        private void materialFlatButton2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog arch = new OpenFileDialog();
+            arch.Title = "Выберите архив для импорта текста из него";
+            arch.Filter = "Archive files(*.rar, *.zip)|*.zip|Text files (*.txt)|*.txt|All files(*.*)|*.*";
+            if (arch.ShowDialog() == DialogResult.OK)
+            {
+                string[] fileType = arch.FileName.Select(s => arch.FileName.Remove(0, arch.FileName.LastIndexOf('.') + 1)).ToArray();
+                string[] fName = arch.FileName.Split('\\');
+                
+                if (fileType.First().ToLower() == "zip" || fileType.First().ToLower() == "txt")
+                {
+                    if (fileType.First().ToLower() == "zip")
+                    {
+                        int dots = 0;
+                        for (int i = 0; i < fName.Length; i++)
+                        {
+                            if (fName.Last()[i] == '.')
+                            {
+                                dots++;
+                            }
+                            if (dots > 1)
+                            {
+                                MessageBox.Show("Ошибка. В имени архива больше одной точки. Пожалуйста, переименуйте архив.");
+                                return;
+                            }
+                        }
+
+                        string[] fileSOnDot = fName.Last().Split('.');
+                        try
+                        {
+                            Directory.CreateDirectory("Archives\\" + fileSOnDot.First());
+                            try
+                            {
+                                ZipFile.ExtractToDirectory(arch.FileName, @"Archives\\" + fileSOnDot.First() + "\\");
+                            }
+                            catch(Exception)
+                            {
+                                MessageBox.Show("Не удалось открыть архив.");
+                            }
+                        }
+                        catch (IOException)
+                        {
+                            MessageBox.Show("Ошибка. Такой архив уже импортировался.");
+                            return;
+                        }
+
+                        string[] files = Directory.GetFiles(@"Archives\", "*.txt", SearchOption.AllDirectories);
+
+                        foreach (string file in files)
+                        {
+                            StreamReader readF = new StreamReader(file);
+                            textBox1.Text += "\r\n" + readF.ReadToEnd() + "\r\n";
+                            readF.Close();
+                        }
+
+                        Directory.Delete("Archives\\" + fileSOnDot.First(), true);
+                        MessageBox.Show("Succcess", "FormatCParser");
+                    }
+                    else if (fileType.First().ToLower() == "txt")
+                    {
+                        StreamReader readF = new StreamReader(arch.FileName);
+                        textBox1.Text += "\r\n" + readF.ReadToEnd() + "\r\n";
+                        readF.Close();
+                        MessageBox.Show("Success", "FormatCParser");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Расширение файла не поддерживается.");
+                }
+            }
+        }
+
+        private void materialFlatButton1_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void materialFlatButton3_Click(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true) { textBox2.Text += "\r\n\r\n[" + DateTime.Now + "]\r\n"; }
+
+            if (textBox1.Text.Trim() != "" && materialSingleLineTextField1.Text.Trim() != "")
+            {
+                foreach (var lines in textBox1.Text.Split(new[] 
+                {
+                    Environment.NewLine
+                }, StringSplitOptions.RemoveEmptyEntries))
+                {
+                    try
+                    {
+                        if (lines.ToLower().Contains(materialSingleLineTextField1.Text.ToLower()) == true)
+                        {
+                            string[] text = lines.Split('|');
+                            if (checkBox1.Checked == true)
+                            {
+                                if (text.First().ToLower().Contains("https") == true && text.First().ToLower().Contains("http") == true)
+                                {
+                                    textBox2.Text += "\r\n" + text[1].Replace(" ", "") + ":" + text.Last().Replace(" ", "");
+                                }
+                            }
+                            else
+                            {
+                                if (text.First().ToLower().Contains("https") == true || text.First().ToLower().Contains("http") == true)
+                                {
+                                    textBox2.Text += "\r\n" + text[1].Replace(" ", "") + ":" + text.Last().Replace(" ", "") + "\r\n";
+                                }
+                                else
+                                {
+                                    textBox2.Text += "\r\n" + lines.Replace(" ", "") + "\r\n";
+                                }
+                            }
+                        }
+                    }
+                    catch (Exception) { }
+                }
+            }
+            else
+            {
+                MessageBox.Show("Text boxes is empty.");
+            }
+        }
+
+        private void materialFlatButton4_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = "";
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked == true)
+            {
+                MessageBox.Show("Включены только те результаты, у которых есть \"https\" или \"http\"");
+            }
+            else
+            {
+                MessageBox.Show("Включены все результаты");
+            }
+        }
+
+        private void materialFlatButton1_Click_1(object sender, EventArgs e)
+        {
+            if (textBox2.Text.Trim() != "")
+            {
+                SaveFileDialog saveF = new SaveFileDialog();
+                saveF.Title = "Сохранение вывода";
+                saveF.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+                if (saveF.ShowDialog() == DialogResult.OK)
+                {
+                    StreamWriter saveFile = new StreamWriter(saveF.FileName);
+                    saveFile.WriteLine(textBox2.Text);
+                    saveFile.Close();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Text box is empty.");
+            }
+        }
+
+        private void materialFlatButton5_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(
+                "FormatC:  Привет! Ты заинтересовался кто создал эту программу?\n" +
+                "Не сомневайся, её создал - FormatC или Armadillo-CLD, называй как хочешь :D\n" +
+                "Мои контакты: \n" +
+                "Github: https://github.com/armadillo-cld\n" +
+                "VK: https://vk.com/sha1.encryption\n" +
+                "Telegram: @cyber_andrey\n" +
+                "Приятного использования :)");
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+        }
+
+        private void materialFlatButton6_Click(object sender, EventArgs e)
+        {
+            textBox2.Text = "";
+        }
+
+        private void checkBox2_CheckedChanged_1(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked == true)
+            {
+                MessageBox.Show("Текущая дата включена");
+            }
+            else
+            {
+                MessageBox.Show("Текущая дата выключена");
+            }
+        }
+    }
+}
